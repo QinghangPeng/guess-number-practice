2250a92f6d347d64b8380180aca6fe644ebd974c
kbyyd24 <melo@gaoyuexiang.cn>
2020-01-19 19:57:12 +0800

    refactor, use constant

--- src/main/java/com/thoughtworks/school/practice/guessnumber/Answer.java
+++ src/main/java/com/thoughtworks/school/practice/guessnumber/Answer.java
@@ -5,6 +5,7 @@ import java.util.stream.IntStream;
 
 public class Answer {
 
+  private static final int ANSWER_SIZE = 4;
   private List<Character> answer;
 
   public Answer(List<Character> answer) {
@@ -12,15 +13,15 @@ public class Answer {
   }
 
   public CheckResult check(List<Character> guessNumber) {
-    if (guessNumber.stream().distinct().count() != 4) {
+    if (guessNumber.stream().distinct().count() != ANSWER_SIZE) {
       throw new RuntimeException();
     }
 
-    int correctCount = (int) IntStream.range(0, 4)
+    int correctCount = (int) IntStream.range(0, ANSWER_SIZE)
         .filter(idx -> answer.get(idx).equals(guessNumber.get(idx)))
         .count();
 
-    int wrongPositionCount = (int) IntStream.range(0, 4)
+    int wrongPositionCount = (int) IntStream.range(0, ANSWER_SIZE)
         .filter(idx -> answer.contains(guessNumber.get(idx)))
         .filter(idx -> !answer.get(idx).equals(guessNumber.get(idx)))
         .count();
